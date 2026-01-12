6d73cd1d5f26925ea951cd462fd37529ad8d4c80
techworld7862283 <techworld7862283@gmail.com>
2026-01-12 13:33:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if not st.session_state.logged_in:
 
 if st.sidebar.button("Logout"):
     st.session_state.logged_in = False
-    st.experimental_rerun()
+    st.rerun()
 
 # ======================
 # UPLOAD RESUME
